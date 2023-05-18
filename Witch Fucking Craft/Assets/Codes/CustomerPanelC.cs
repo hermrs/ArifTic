8dd383117bdc3feb6d1c7dfcf0a68fba2806ebd9
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2023-05-18 10:23:52 +0300

    customer menu
    customer menu

---
+++ Witch Fucking Craft/Assets/Codes/CustomerPanelC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerPanelC : MonoBehaviour
+{
+    [SerializeField]
+    public Image vesikalýk;
+    public Text cusÝsim;
+    public Text cusÝstenen;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
