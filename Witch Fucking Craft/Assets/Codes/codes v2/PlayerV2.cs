54252c07ef5009c8ff848b00ef2d17cb2aa7e3c0
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2023-06-16 17:40:55 +0300

    Lots Of Things
    -Yeni Giriş Sistemi
    -Artık Playsatation ve Xbox kolu destekliyor.
    - 3 Farklı Sanal Kamera ( Aktif olarak değiştirilebilir 2 adet 1 adet kitap için sabit).
    -Birçok bug düzeltildi.

--- Witch Fucking Craft/Assets/Codes/codes v2/PlayerV2.cs
+++ Witch Fucking Craft/Assets/Codes/codes v2/PlayerV2.cs
@@ -6,31 +6,14 @@ using UnityEngine;
 
 public class PlayerV2 : MonoBehaviour
 {
-    private bool isWalking; 
-    [SerializeField]
-    private float playerSpeed = 7f;
-    private float rotateSpeed = 12f;
+    private bool isWalking;
+
+    [SerializeField]  private GameInputV2 gameInput;
+    [SerializeField]  private float playerSpeed = 7f;
+    [SerializeField]  private float rotateSpeed = 12f;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0,0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
+       Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
         transform.position += moveDir * Time.deltaTime * playerSpeed;
         isWalking= moveDir != Vector3.zero;
